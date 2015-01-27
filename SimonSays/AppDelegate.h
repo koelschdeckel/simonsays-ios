9ec50da858f97c52e86127aefd4f4709f693f125
Matthias <mattikra@gmail.com>
2015-01-27 01:31:21 +0100

    Initial Commit

---
+++ SimonSays/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimonSays
+//
+//  Created by Matthias Krauß on 27.01.15.
+//  Copyright (c) 2015 Matthias Krauß. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
